2ddf4905a42abc06f1b9897bd156264fb3e2aae8
Silviu Miron <0miros68@solent.ac.uk>
2025-10-20 23:01:38 +0100

    Create activitiy preform progrm

--- week2/if_statements.py
+++ week2/if_statements.py
@@ -5,3 +5,14 @@ book_type = input("What type of book is this?\n")
 if  book_type.lower() == 'adventure':
   print("I like adventure books!")
 print("Finished reading book")
+
+
+print(f"\n")
+
+activity = input("Please enter and activity: \n")
+
+if activity.lower() == 'calculate':
+  print("Performing Calculation...")
+else:
+  print("Performing activity...")
+print("Activity completed!")
