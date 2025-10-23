5b5e43ca648c26655d21b2b139cdcfe9447c4980
Silviu Miron <0miros68@solent.ac.uk>
2025-10-23 15:31:43 +0100

    Added code to demonstrate the use of an if... elif...else statement

--- week2/if_statements.py
+++ week2/if_statements.py
@@ -1,19 +1,42 @@
 
 
-book_type = input("What type of book is this?\n")
+#If
 
-if  book_type.lower() == 'adventure':
-  print("I like adventure books!")
-print("Finished reading book")
+# book_type = input("What type of book is this?\n")
+#
+# if  book_type.lower() == 'adventure':
+#   print("I like adventure books!")
+# print("Finished reading book")
+#
+#
+# print(f"\n")
+#
+# activity = input("Please enter and activity: \n")
 
 
-print(f"\n")
 
-activity = input("Please enter and activity: \n")
+#IF ELSE
 
-if activity.lower() == 'calculate':
-  print("Performing Calculation...")
+# if activity.lower() == 'calculate':
+#   print("Performing Calculation...")
+# else:
+#   print("Performing activity...")
+# print("Activity completed!")
+# print("Activity completed!")
+
+#IF ELIF
+
+directions = input("Towards which direction should I go (up,down, left or right)? \n")
+
+if directions == "up":
+  print("I am moving in an upward direction")
+elif directions == "down":
+  print("I am moving in an downward direction")
+elif directions == "left":
+  print("I am moving in an left direction")
+elif directions == "right":
+  print("I am moving in an right direction")
 else:
-  print("Performing activity...")
-print("Activity completed!")
-print("Activity completed!")
+  print("Choose one of the directions")
+
+
