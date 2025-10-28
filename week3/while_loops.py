1c8c1c57b0fce7328c50d23f02f2a0948a3d3bf2
Silviu Miron <0miros68@solent.ac.uk>
2025-10-28 23:06:25 +0000

    Added code to demonstrate the use of a while loop with a counter

--- week3/while_loops.py
+++ week3/while_loops.py
@@ -1,11 +1,28 @@
 
 
-apple_removed = int(input("How many apples should I remove?\n"))
+# apple_removed = int(input("How many apples should I remove?\n"))
+#
+# total_apple = 0
+# string = "Remove apple"
+#
+# while total_apple < apple_removed:
+#     print(string)
+#     total_apple +=1
 
-total_apple = 0
-string = "Remove apple"
 
-while total_apple < apple_removed:
-    print(string)
-    total_apple +=1
+# Task 2
 
+#Create an input variable
+obstacles = int(input("How many obstacles must avoid ?\n"))
+
+#Define new variable
+obstacles_number = 0
+
+# while loop
+while obstacles_number < obstacles:
+    obstacles_number +=1
+    print(f"Avoiding...Done!  {obstacles_number} obstacle avoided",end="")
+
+#Printing final message
+print("\n")
+print("All obstacles have been avoided.")
