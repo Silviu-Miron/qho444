63d068e213276ff32514cb1b56a72cdad1c844f8
Silviu Miron <silviu_miron_90@outlook.com>
2025-10-29 22:52:59 +0000

    Added code to demonstrate counting down with a for loop

--- for_loop.py
+++ for_loop.py
@@ -3,21 +3,30 @@
 
 #Activity 1: Simple Loop
 
-mountains = int(input("How many mountains should I display?"))
+# mountains = int(input("How many mountains should I display?"))
+#
+# mountains_art = r"""
+#    __
+#   /  \_
+#  /^    \
+# /  ^    \_
+# _/ ^ ^     ^\
+# /  ^     ^    \
+# """
+#
+# print("Displaying...")
+# for i in range(mountains):
+#     i+=1
+#     print(mountains_art)
+#
+# print("Done!")
 
-mountains_art = r"""
-   __
-  /  \_
- /^    \
-/  ^    \_
-_/ ^ ^     ^\
-/  ^     ^    \
-"""
+# Activity 2: Count Down
 
-print("Displaying...")
-for i in range(mountains):
-    i+=1
-    print(mountains_art)
+steps = int(input("How far are we from the target?\n"))
 
-print("Done!")
+for i in range(steps,0,-1):
+
+    print(f"{i} step remaining")
+
 
