936e4f69308d9cab8753adf16aec508d33cca542
Silviu Miron <silviu_miron_90@outlook.com>
2025-11-04 16:09:10 +0000

    Added code to demonstrate the use of nested loops and decisions

--- week3/nested_loop.py
+++ week3/nested_loop.py
@@ -4,13 +4,27 @@
 
 #Activity 1
 
-rows = int(input("How many rows would you like ?\n"))
+# rows = int(input("How many rows would you like ?\n"))
+#
+# columns = int(input("How many columns would you like ?\n"))
+#
+#
+# for row in range(rows):
+#     for column in range (columns):
+#         print(":-)",end="")
+#     print()
 
-columns = int(input("How many columns would you like ?\n"))
 
+#################################################
 
-for row in range(rows):
-    for column in range (columns):
-        print(":-)",end="")
-    print()
+#Activity 2 Nesting
 
+character = input("Enter a sequence of characters consisting of dashes and two markers \n")
+
+marker = input("Enter a character representing a marker \n")
+
+for character in character:
+    print(character, end="")
+    for marker in marker:
+        print(marker, end="")
+print(character)
