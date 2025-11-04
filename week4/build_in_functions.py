99447cb1be6ba385fe56ae43fc02428cbb46dae0
Silviu Miron <silviu_miron_90@outlook.com>
2025-11-04 22:37:45 +0000

    Added code to demonstrate the use of  a user-defined function with a loop

--- week4/build_in_functions.py
+++ week4/build_in_functions.py
@@ -12,20 +12,20 @@
 # else:
 #     print("Error message")
 
-print("Program Ended!")
+# print("Program Ended!")
 ########################################################
 
 #ASCII Character
 
-print("Program Started!")
-
-ascii_code = int(input("Please enter an ASCII code: "))
-
-if ascii_code in range(32,127):
-        character = chr(ascii_code)
-        print( print(f"The character represented by the ASCII code is {character}"))
-else:
-        print("Error! The number is not printable ASCII code.")
-
-print("Program Ended!")
+# print("Program Started!")
+#
+# ascii_code = int(input("Please enter an ASCII code: "))
+#
+# if ascii_code in range(32,127):
+#         character = chr(ascii_code)
+#         print( print(f"The character represented by the ASCII code is {character}"))
+# else:
+#         print("Error! The number is not printable ASCII code.")
+#
+# print("Program Ended!")
 
--- week4/custom_functions.py
+++ week4/custom_functions.py
@@ -34,31 +34,49 @@
 #Activity 3 : Parameters
 
 
-def listen ():
-    word = input("Enter a word: ")
-    print(f"That what a loud {word}")
+# def listen ():
+#     word = input("Enter a word: ")
+#     print(f"That what a loud {word}")
+#
+# listen()
+#
+# def adventure_game():
+#     word = input("Enter a word representing what you see:")
+#     if word.lower() == "a large boulder":
+#         print("It's time to run!")
+#     else:
+#         print("We will be fine")
+# adventure_game()
+#
+#
+# def escape_play(plan):
+#     if plan  ==  "jumping over":
+#         print("We cannot escape that way! The boulder is too big!")
+#     elif plan == "running around":
+#         print("We cannot escape that way! The boulder is moving too fast!")
+#     elif plan == "cross bridge ahead":
+#         print("That might just work! Let's go!")
+#     else:
+#         print("We cannot escape that way! The boulder is in the way!")
+#
+# escape_play("jumping over")
+# escape_play("running around")
+# escape_play("cross bridge ahead")
 
-listen()
 
-def adventure_game():
-    word = input("Enter a word representing what you see:")
-    if word.lower() == "a large boulder":
-        print("It's time to run!")
-    else:
-        print("We will be fine")
-adventure_game()
+#############################################
 
+#Activity 4 : LOOP
 
-def escape_play(plan):
-    if plan  ==  "jumping over":
-        print("We cannot escape that way! The boulder is too big!")
-    elif plan == "running around":
-        print("We cannot escape that way! The boulder is moving too fast!")
-    elif plan == "cross bridge ahead":
-        print("That might just work! Let's go!")
-    else:
-        print("We cannot escape that way! The boulder is in the way!")
+def cross_bridge (steps):
+    distance = 0
+    for distance in range(steps):
+        print("Crossed step")
+    if distance == 5:
+        print("Bridge is collapsing!")
 
-escape_play("jumping over")
-escape_play("running around")
-escape_play("cross bridge ahead")
+    if distance < 5 :
+        print("We must keep going!")
+
+cross_bridge(3)
+cross_bridge(6)
