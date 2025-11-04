b41c2bc3881d9f2831020f6f4db6705866965ec2
Silviu Miron <silviu_miron_90@outlook.com>
2025-11-04 19:03:27 +0000

    Added code to demonstrate the use of built-in functions:abs, range, char

--- week4/build_in_functions.py
+++ week4/build_in_functions.py
@@ -1,8 +1,31 @@
+
+#####################################################
+
+#ASCII Values
+
+# print("Program Started!")
+#
+# letter = input("Please enter a standard character: ")
+#
+# if len(letter) == 1:
+#     print(f"The ascii code for {letter} is {ord(letter)} ")
+# else:
+#     print("Error message")
+
+print("Program Ended!")
+########################################################
+
+#ASCII Character
+
 print("Program Started!")
 
-letter = input("Please enter a standard character: ")
+ascii_code = int(input("Please enter an ASCII code: "))
 
-if len(letter) == 1:
-    print(f"The ascii code for {letter} is {ord(letter)} ")
+if ascii_code in range(32,127):
+        character = chr(ascii_code)
+        print( print(f"The character represented by the ASCII code is {character}"))
 else:
-    print("Error message")
+        print("Error! The number is not printable ASCII code.")
+
+print("Program Ended!")
+
