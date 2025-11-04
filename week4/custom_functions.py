ec4411d4713f749c939b09b389a64e312de91209
Silviu Miron <silviu_miron_90@outlook.com>
2025-11-04 19:26:53 +0000

    Added code to demonstrate the use of a user-defined function with a nested decision

--- week4/custom_functions.py
+++ week4/custom_functions.py
@@ -1,6 +1,22 @@
 
-def listen ():
-    word = input("Enter a word: ")
-    print(f"That what a loud {word}")
 
-listen()
+###########################################
+
+#Activity 1 : Simple User Defined Function
+# def listen ():
+#     word = input("Enter a word: ")
+#     print(f"That what a loud {word}")
+#
+# listen()
+
+#############################################
+
+#Activity 2 : Nesting
+
+def adventure_game():
+    word = input("Enter a word representing what you see:")
+    if word.lower() == "a large boulder":
+        print("It's time to run!")
+    else:
+        print("We will be fine")
+adventure_game()
