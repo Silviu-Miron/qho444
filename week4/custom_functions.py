514722fac8d3a0a7b7d9e710ca03d1db3b5c7137
Silviu Miron <silviu_miron_90@outlook.com>
2025-11-04 19:43:32 +0000

    Added code to demonstrate the use of a use-defined function with a parameter

--- week4/custom_functions.py
+++ week4/custom_functions.py
@@ -13,6 +13,33 @@
 
 #Activity 2 : Nesting
 
+#Activity 1 : Simple User Defined Function
+# def listen ():
+#     word = input("Enter a word: ")
+#     print(f"That what a loud {word}")
+#
+# listen()
+
+# def adventure_game():
+#     word = input("Enter a word representing what you see:")
+#     if word.lower() == "a large boulder":
+#         print("It's time to run!")
+#     else:
+#         print("We will be fine")
+# adventure_game()
+
+
+#############################################
+
+#Activity 3 : Parameters
+
+
+def listen ():
+    word = input("Enter a word: ")
+    print(f"That what a loud {word}")
+
+listen()
+
 def adventure_game():
     word = input("Enter a word representing what you see:")
     if word.lower() == "a large boulder":
@@ -20,3 +47,18 @@ def adventure_game():
     else:
         print("We will be fine")
 adventure_game()
+
+
+def escape_play(plan):
+    if plan  ==  "jumping over":
+        print("We cannot escape that way! The boulder is too big!")
+    elif plan == "running around":
+        print("We cannot escape that way! The boulder is moving too fast!")
+    elif plan == "cross bridge ahead":
+        print("That might just work! Let's go!")
+    else:
+        print("We cannot escape that way! The boulder is in the way!")
+
+escape_play("jumping over")
+escape_play("running around")
+escape_play("cross bridge ahead")
