1ea8524292985c4126749b0b2253caedd835d1b6
Silviu Miron <silviu_miron_90@outlook.com>
2025-11-06 22:27:07 +0000

    Added code to demonstrate the use of return values

--- multiple_user_defined_functions.py
+++ multiple_user_defined_functions.py
@@ -5,17 +5,48 @@
 
 #Activity 1
 
-def display_ladder(steps):
+# def display_ladder(steps):
+#
+#    for _ in range(steps):
+#        print("""| |
+#                 ***""")
+#
+# def create_ladder():
+#     steps = int (input("How many steps would you like?"))
+#     display_ladder(steps)
+#
+# create_ladder()
 
-   for _ in range(steps):
-       print("""| |
-                ***""")
-
-def create_ladder():
-    steps = int (input("How many steps would you like?"))
-    display_ladder(steps)
-
-create_ladder()
 
 
+##########################################
 
+#Activity 2 : Return Values
+
+def sum_weights(char_weight, char_inventory):
+    total =  char_weight + char_inventory
+    return total
+
+
+def calc_avg_weight(char_weight, char_inventory):
+    total= sum_weights(char_weight, char_inventory)
+    total/=2
+    return total
+
+def run():
+    person_weight = float(input("What is the weight of the person?\n"))
+    inventory_weight = float(input("What is the weight of the inventory?\n"))
+    message = input("Please enter the word: sum or average ").lower()
+
+    if message == "sum":
+        total = sum_weights(person_weight, inventory_weight)
+        print("The total is: ", total)
+    elif message == "average":
+        total = calc_avg_weight(person_weight, inventory_weight)
+        print("The total is: ", total)
+    else:
+        print("Please enter a valid input.")
+
+
+
+run()
