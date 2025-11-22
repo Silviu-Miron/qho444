e7ef6972a80c36b1f4c575c9811d9156502a9682
Silviu Miron <silviu_miron_90@outlook.com>
2025-11-22 13:33:06 +0000

    Added code to extract data from the CSV file

--- week6/csv_tasks.py
+++ week6/csv_tasks.py
@@ -1,23 +1,53 @@
+# import csv
+
+
+# def read_csv(file_path):
+#     file = open(file_path, "r")
+#     csv_reader = csv.reader(file)
+#     headings = next(csv_reader)
+#     print("Headings:")
+#     print(headings)
+#
+#     print("Values:")
+#     for values in csv_reader:
+#         print(values)
+#     file.close()
+#
+#
+# def run_task1():
+#     read_csv("clothing.csv")
+#
+#
+# if __name__ == "__main__":
+#     run_task1()
+
+####################################
+
+# Activity 2
+
 import csv
-import sys
 
 
-def read_csv(file_path):
+def extract(file_path):
+    print("Extracting: ", end="")
     file = open(file_path, "r")
     csv_reader = csv.reader(file)
-    headings = next(csv_reader)
-    print("Headings:")
-    print(headings)
-
-    print("Values:")
+    next(csv_reader)
+    names = ""
     for values in csv_reader:
-        print(values)
+        items_names = values[1]
+
+        names += items_names + "\n"
     file.close()
 
+    print("Done")
+    print("The extracted items are: ")
+    print(names.strip())
 
-def run_task1():
-    read_csv("clothing.csv")
+
+def run_task2():
+    extract("clothing.csv")
 
 
 if __name__ == "__main__":
-    run_task1()
+    run_task2()
