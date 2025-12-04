a7b5f3d2183949323a9f58b2fb71f6dd811c9818
Silviu Miron <silviu_miron_90@outlook.com>
2025-12-04 20:49:05 +0000

    Added code to demonstrate sort

--- set_tasks.py
+++ set_tasks.py
@@ -13,28 +13,71 @@
 # Task 2
 
 
+# def observed_items():
+#     observations = []
+#
+#     for _ in range(7):
+#         item = input("Please enter an observation:\n")
+#         observations.append(item)
+#
+#     return observations
+#
+#
+# def run_task2():
+#
+#     print("Counting observations...")
+#     data = observed_items()
+#     results = set()
+#     for item in data:
+#         cont = data.count(item)
+#         results.add((item, cont))
+#
+#     for item, cont in results:
+#         print(f"{item} observed: {cont} times")
+#
+#
+# run_task2()
+
+
+# Activity 3
+
+
 def observed_items():
     observations = []
 
-    for _ in range(7):
+    for _ in range(5):
         item = input("Please enter an observation:\n")
         observations.append(item)
 
-        return observations
-    return None
+    return observations
 
 
-def run_task2():
+def remove_observations(obs_list):
+    while True:
+        choice = (
+            input("\nDo you wish to remove an observation (yes/no) ").strip().lower()
+        )
+        if choice != "yes":
+            break
 
-    print("Counting observations...")
+        to_remove = input("\n What observation do you wish to remove?\n")
+        if to_remove in obs_list:
+            obs_list.remove(to_remove)
+            print("Done")
+
+        else:
+            print("That observation does not exist")
+
+
+def run_task3():
     data = observed_items()
-    results = set()
-    for item in data:
-        cont = data.count(item)
-        results.add((item, cont))
+    remove_observations(data)
 
-    for item, cont in results:
-        print(f"{item} observed: {cont} times")
+    print("\n Observations:")
+    sorted_items = sorted(set(data))
+    for item in sorted_items:
+        count = data.count(item)
+        print(f"{item} observed: {count} times")
 
 
-run_task2()
+run_task3()
