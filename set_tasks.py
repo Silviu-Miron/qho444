d73e58889bd797af40c238101b28d95f06a7caff
Silviu Miron <silviu_miron_90@outlook.com>
2025-12-04 20:27:17 +0000

    Added code to demonstrate how to populate a set

--- set_tasks.py
+++ set_tasks.py
@@ -1,10 +1,40 @@
-def observed():
-    observations = {"Car", "Sky", "Sky Scraper", "Bike", "House", "House"}
-    return observations
+# def observed():
+#     observations = {"Car", "Sky", "Sky Scraper", "Bike", "House", "House"}
+#     return observations
+#
+#
+# def run_task1():
+#     print(observed())
+#
+#
+# run_task1()
 
 
-def run_task1():
-    print(observed())
+# Task 2
 
 
-run_task1()
+def observed_items():
+    observations = []
+
+    for _ in range(7):
+        item = input("Please enter an observation:\n")
+        observations.append(item)
+
+        return observations
+    return None
+
+
+def run_task2():
+
+    print("Counting observations...")
+    data = observed_items()
+    results = set()
+    for item in data:
+        cont = data.count(item)
+        results.add((item, cont))
+
+    for item, cont in results:
+        print(f"{item} observed: {cont} times")
+
+
+run_task2()
