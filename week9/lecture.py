1fc82b4f7e97b02262cffe5b7af273a15592ed3b
Silviu Miron <silviu_miron_90@outlook.com>
2025-12-09 20:59:47 +0000

    Added visualisation code and install matplotlib

---
+++ week9/lecture.py
@@ -0,0 +1,83 @@
+#%%
+
+
+# from matplotlib import pyplot as plt
+
+# x = [0,2,4,6,10]
+# y = [0,20,40,60,100]
+#
+# plt.xlabel('x values')
+# plt.ylabel('y values')
+# plt.bar(x,y)
+# plt.show()
+
+# from matplotlib import pyplot as plt
+#
+# labels = ("A","B","C","D")
+# size = [15, 30,45,10]
+#
+# plt.pie(size, labels=labels)
+# plt.show()
+
+
+#Task
+
+# A local fitness club recorded how many people visited the gym during the last 6 days:
+#
+# | Day | Visitors |
+# | --- | -------- |
+# | Mon | 55       |
+# | Tue | 73       |
+# | Wed | 68       |
+# | Thu | 80       |
+# | Fri | 120      |
+# | Sat | 150      |
+# QW
+# QAHE Webex045
+# qa.com
+# 8:25 PM
+# Create 3 different visualisations using Matplotlib:
+#
+# Line Chart
+#
+# Plot visitors per day as a line graph.
+#
+# Bar Chart
+#
+# Plot the same data as a bar chart.
+#
+# Pie Chart
+#
+# Create a pie chart showing the percentage share of visitors for each day.
+
+from matplotlib import pyplot as plt
+
+
+x = ["Mon","Tue","Wed","Thu","Fri","Sat"]
+y=[55, 73, 68, 80, 120, 150]
+
+plt.xlabel('x values')
+plt.ylabel('y values')
+plt.bar(x,y)
+plt.show()
+
+from matplotlib import pyplot as plt
+
+labels = ["Mon","Tue","Wed","Thu","Fri","Sat"]
+size = [55, 73, 68, 80, 120, 150]
+
+plt.pie(size, labels=labels)
+plt.show()
+
+import matplotlib.pyplot as plt
+
+x=["Monday " , "Tuesday", "Wednesday ", "Thursday", "Friday", "Saturday"]
+y=[55,73,68,80,120,150]
+
+
+plt.xlabel("x values")
+plt.ylabel("y values")
+plt.plot(x, y)
+plt.show()
+
+#%%
